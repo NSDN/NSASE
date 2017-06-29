e295124f38217a22c2684188abe52f33ce63f0d0
zzm32 Dr <zzm32 Dr>
2017-06-29 17:44:24 +0800

    init

---
+++ NSASE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using dotNSASM;
+
+namespace NSASE
+{
+    class NSASE : NSASM
+    {
+        public NSASE(string[][] code) : base(2048, 1024, 64, code)
+        {
+        }
+
+        protected override void LoadFunList()
+        {
+            base.LoadFunList();
+
+        }
+    }
+}
